98c404bb729a9935b0153e5457d0c2c1426da2bc
Christopher A. Williamson <home@chrisaw.com>
2021-04-09 20:17:00 +0100

    Fix for c2q (from c2s)

--- common/scripts/1_fetch_kernel.sh
+++ common/scripts/1_fetch_kernel.sh
@@ -3,5 +3,5 @@
 # fetch kernel sources
 git clone \
   -b ${1:-master} \
-  https://github.com/cawilliamson/android_kernel_samsung_c2s \
+  https://github.com/cawilliamson/android_kernel_samsung_c2q \
   /usr/src/kernel
--- common/scripts/2_build_kernel.sh
+++ common/scripts/2_build_kernel.sh
@@ -10,7 +10,7 @@ ln -s /opt/toolchain /usr/src/kernel/toolchain
 ANDROID_MAJOR_VERSION=r
 PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION PLATFORM_VERSION
-make -j$(nproc --all) ARCH=arm64 exynos9830-c2sxxx_defconfig
+make -j$(nproc --all) ARCH=arm64 c2q_chn_openx_defconfig
 make -j$(nproc --all) ARCH=arm64
 
 # remove previous images
